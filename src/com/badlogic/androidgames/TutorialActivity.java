8b30dda27495457e22d3fb132d2d8d6901e5e976
Gordin <9ordin@gmail.com>
2011-12-03 17:19:23 +0100

    initial commit

---
+++ src/com/badlogic/androidgames/TutorialActivity.java
@@ -0,0 +1,13 @@
+package com.badlogic.androidgames;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TutorialActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ src/com/badlogic/androidgames/framework/impl/AndroidAudio.java
@@ -0,0 +1,61 @@
+package com.badlogic.androidgames.framework.impl;
+
+import java.io.IOException;
+
+import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
+
+import com.badlogic.androidgames.framework.Audio;
+import com.badlogic.androidgames.framework.Music;
+import com.badlogic.androidgames.framework.Sound;
+
+public class AndroidAudio implements Audio {
+    AssetManager assets;
+    SoundPool soundPool;
+    protected AndroidAudio instance;
+
+    private AndroidAudio(Activity activity) {
+        activity.setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        this.assets = activity.getAssets();
+        this.soundPool = new SoundPool(20, AudioManager.STREAM_MUSIC, 0);
+    }
+
+    public AndroidAudio get(Activity activity) {
+        if (instance == null) {
+            instance = new AndroidAudio(activity);
+        }
+        return instance;
+    }
+
+    public AndroidAudio get() {
+        if (instance == null) {
+            throw new RuntimeException("Call get(Activity) first!");
+        }
+        return instance;
+    }
+
+    @Override
+    public Music newMusic(String filename) {
+        try {
+            AssetFileDescriptor assetDescriptor = assets.openFd(filename);
+            return new AndroidMusic(assetDescriptor);
+        } catch (IOException e) {
+            throw new RuntimeException("Couldn't load music '" + filename + "'");
+        }
+    }
+
+    @Override
+    public Sound newSound(String filename) {
+        try {
+            AssetFileDescriptor assetDescriptor = assets.openFd(filename);
+            int soundId = soundPool.load(assetDescriptor, 0);
+            return new AndroidSound(soundPool, soundId);
+        } catch (IOException e) {
+            throw new RuntimeException("Couldn't load sound '" + filename + "'");
+        }
+    }
+
+}
